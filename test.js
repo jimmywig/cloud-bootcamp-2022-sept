f1d154a8c13b762854056a371fbbfd93ee2acddc
james <jamesrosselmes@gmail.com>
2025-09-09 23:11:12 +1000

    test

---
+++ test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
